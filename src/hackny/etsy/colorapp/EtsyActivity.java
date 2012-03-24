a0eef47dd582b0c736a22130adb905bea6cfab9a
Elissa <welissa@seas.upenn.edu>
2012-03-24 17:41:33 -0400

    starting

---
+++ src/hackny/etsy/colorapp/EtsyActivity.java
@@ -0,0 +1,13 @@
+package hackny.etsy.colorapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EtsyActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
