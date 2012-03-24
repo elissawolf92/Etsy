ea51ccd8587c13a6b0059c7c59a0769e3fe245a2
Elissa <welissa@seas.upenn.edu>
2012-03-24 19:24:10 -0400

    lots of stuff

--- src/hackny/etsy/colorapp/EtsyActivity.java
+++ src/hackny/etsy/colorapp/EtsyActivity.java
@@ -1,13 +1,33 @@
 package hackny.etsy.colorapp;
 
+import java.io.File;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class EtsyActivity extends Activity {
+	
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    public void onTakePhotoClicked(View view){
+    	Intent intent = new Intent(this, Camera.class);
+    	startActivity(intent);
+    
+    }
 }
+    
